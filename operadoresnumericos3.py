461caf1862ec679cb670a0724fd40be91293da59
juancampos45 <55142610+juancampos45@users.noreply.github.com>
2019-09-12 10:34:04 -0400

    Add files via upload

---
+++ operadoresnumericos3.py
@@ -0,0 +1,21 @@
+#  Propo8sito: continuando con los operadores aritmeticos
+
+# Entrada de datos
+a = float(input('ingrese el valor A: '))
+b = float(input('ingrese el valor B: '))
+c = float(input('ingrese el valor C: '))
+d = float(input('ingrese el valor D: '))
+
+# Proedimiento
+
+f = (a + b)/(c - d)
+
+# Salida de datos
+
+print(' El resultado de (a+b)/(c-d) es: ', f)
+
+# Fin del programa
+
+input('presione cualquier tecla para continuar...')#pausa
+
+   
